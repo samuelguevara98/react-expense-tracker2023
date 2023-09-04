43ef8b2b02701b960dad2a78b89aa9f1185f0650
Samuel Guevara <samsjamin@outlook.com>
2023-09-04 00:53:34 -0400

    Created stylized header with buttons for app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import React from "react";
+//* import Header from './components/Header'; items will be copied & pasted to component folder
 
 function App() {
-  return <header>
-    <h1 className="text-2xl ">Budget</h1>
+  return (
     <div>
-      <button>Add Budget</button>
-      <button>Add Expense</button>
+      <div className='bg-black p-4 flex justify-evenly items-center '>
+        <h1 className='text-white text-2xl font-semibold'>Budget</h1>
+        <div>
+          <button className='bg-green-900 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mx-5'>
+            Add Earnings
+          </button>
+          <button className='bg-red-900 hover:bg-red-700 text-white font-bold py-2 px-4 rounded'>
+            Add Expenses
+          </button>
+        </div>
+      </div>
     </div>
-  </header>;
+  );
 }
 
 export default App;

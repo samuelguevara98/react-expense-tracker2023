e19c6d49802e0747fdab6ba19bf22781c880e5eb
Samuel Guevara <samsjamin@outlook.com>
2023-09-03 23:27:54 -0400

    test1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      Hello World!
+  return <header>
+    <h1 className="text-2xl ">Budget</h1>
+    <div>
+      <button>Add Budget</button>
+      <button>Add Expense</button>
     </div>
-  );
+  </header>;
 }
 
 export default App;
